adfe4473abf899220ad4f5409a1fe58e23f1d500
Maximilian Wech <maximilian.wech@hotmail.com>
2018-01-13 14:38:45 +0100

    Added class ItemVM

---
+++ CodingDojo5/ViewModel/ItemVM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+
+namespace CodingDojo5.ViewModel
+{
+    public class ItemVM
+    {
+        public string Description { get; set; }
+        public string AgeRecommendation { get; set; }
+        public BitmapImage Image { get; set; }
+        public ObservableCollection<ItemVM> ItemList { get; set; }
+
+        public ItemVM(string description, string ageRecommendation, BitmapImage image)
+        {
+            Description = description;
+            AgeRecommendation = ageRecommendation;
+            Image = image;
+        }
+
+        public void addItem(ItemVM item)
+        {
+            if (ItemList == null) ItemList = new ObservableCollection<ItemVM>();
+            ItemList.Add(item);
+        }
+
+
+
+
+
+    }
+}
